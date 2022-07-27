fc71ee23e2a5cde3e5c5d8cf86fc988eb9b48bc9
Eiji MATSUMOTO <e.mattsan@gmail.com>
2022-07-27 19:31:26 +0900

    initial commit

---
+++ lib/mutable_binary.ex
@@ -0,0 +1,18 @@
+defmodule MutableBinary do
+  @moduledoc """
+  Documentation for `MutableBinary`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MutableBinary.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mutable_binary_test.exs
@@ -0,0 +1,8 @@
+defmodule MutableBinaryTest do
+  use ExUnit.Case
+  doctest MutableBinary
+
+  test "greets the world" do
+    assert MutableBinary.hello() == :world
+  end
+end
